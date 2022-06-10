22a0337fe648b2a35c2ea414f17868f6a4d5bb47
morales <franciscomoralessanchez00@gmail.com>
2022-06-09 21:48:35 -0600

    figuras gemetricas

--- figuras.js
+++ figuras.js
@@ -1,1 +1,23 @@
-console.log("hola mundo");
+// codigo del cuadrado
+
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado) + "cm";
+
+const ariaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El perimetro del cuadrado es: " + ladoCuadrado + "cm^2");
+
+//codigo del triangulo
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    "Los lados del triangulo miden: " 
+    + ladoTriangulo1 + "cm, " 
+    + ladoTriangulo2 + "cm, " 
+    + baseTriangulo + "cm" 
+    );
