2cc492a8e037f8f122c47264c3f3c0e35208aaf0
morales <franciscomoralessanchez00@gmail.com>
2022-06-19 22:58:30 -0600

    Calculando la mediana de salario

---
+++ salario.js
@@ -0,0 +1,45 @@
+const Nicaragua = [];
+Nicaragua.push({
+    name: "Juanita",
+    salary: 1000,
+});
+Nicaragua.push({
+    name: "Pedro",
+    salary: 800,
+});
+Nicaragua.push({
+    name: "Julio",
+    salary: 900,
+});
+Nicaragua.push({
+    name: "Alberto",
+    salary: 1300,
+});
+Nicaragua.push({
+    name: "Juniet",
+    salary: 1400,
+});
+Nicaragua.push({
+    name: "Elida",
+    salary: 900,
+});
+Nicaragua.push({
+    name: "Juanito",
+    salary: 1100,
+});
+Nicaragua.push({
+    name: "Luisito",
+    salary: 800,
+});
+Nicaragua.push({
+    name: "Mario",
+    salary: 1200,
+});
+Nicaragua.push({
+    name: "Lucia",
+    salary: 1100,
+});
+Nicaragua.push({
+    name: "Pedro",
+    salary: 900,
+});
